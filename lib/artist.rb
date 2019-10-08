cec7638fb992e02b3716e71f1d276c8ebe48255c
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 13:19:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
   end 
   
   def genres 
-    Song.all.collect {|song| song.artist == self ? song.genre} 
+    Song.all.collect {|song| song.genre if song.artist == self}
   end 
   
 end 
