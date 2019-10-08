b70631412f9ae6b968c70ba3ff28ece83f120967
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 13:15:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,14 @@ class Song
   @@all = []
   
   def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
     
+  end 
+  
+  def self.all 
+    @@all 
   end 
   
 end 
